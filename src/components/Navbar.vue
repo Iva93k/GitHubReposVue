751b8ca98fe666be7551251f5c337eaa37ca4b9c
ivanawetek <ivana.kovacevic@wetek.com>
2020-09-13 21:35:31 +0200

    Routing and navigation; bootstrap-vue; navbar; not found; history mode

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-navbar toggleable="md"
+            type="dark"
+            variant="info">
+    <b-navbar-brand :to = "{name: 'Home'}">GitHub Repository Home</b-navbar-brand>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
